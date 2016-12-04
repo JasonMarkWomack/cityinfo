a63d96acb88e681bef0bdf50c2529206efb99b43
JasonMarkWomack <jwomack155@gmail.com>
2016-12-03 23:51:09 -0500

    update/ not complete

--- js-main/controller.js
+++ js-main/controller.js
@@ -1,4 +1,4 @@
-angular.module("app").controller("controller", function($scope, service){
+angular.module("app").controller("controller", function($scope, service, serviceBerlin, serviceTokyo){
 
     $scope.getInfo = function() {
     service.getInfo().then(function(dataFromService) {
@@ -19,33 +19,63 @@ angular.module("app").controller("controller", function($scope, service){
       $scope.ventureCapital = dataFromService.categories[3];
       console.log($scope.ventureCapital);
 
-    //    $scope.housing = dataFromService.categories[0];
-      //  console.log($scope.housing);
-
-
-        //$scope.housing = dataFromService.categories[0];
-        //console.log($scope.housing);
-
-
-        //$scope.housing = dataFromService.categories[0];
-        //console.log($scope.housing);
-
-
-        //$scope.housing = dataFromService.categories[0];
-        //console.log($scope.housing);
       });
     };
 
-    $scope.submit = function() {
-      $scope.arr.push($scope.text);
-
-    };
-    $scope.arr = [];
-
 
   $scope.getInfo();
-  $scope.awesomeClicked = function(){
-    $scope.isAwesome = !$scope.isAwesome;
-  };
+
+      $scope.getInfoBerlin = function() {
+      serviceBerlin.getInfoBerlin().then(function(dataFromService) {
+          console.log(dataFromService);
+
+
+          $scope.housingBerlin = dataFromService.categories[0];
+          console.log($scope.housingBerlin);
+
+          $scope.costOfLivingBerlin = dataFromService.categories[1];
+          console.log($scope.cost0fLivingBerlin);
+
+
+          $scope.startupsBerlin = dataFromService.categories[2];
+          console.log($scope.startupsBerlin);
+
+
+        $scope.ventureCapitalBerlin = dataFromService.categories[3];
+        console.log($scope.ventureCapitalBerlin);
+
+        });
+      };
+
+
+    $scope.getInfoBerlin();
+
+    $scope.getInfoTokyo = function() {
+    serviceTokyo.getInfoTokyo().then(function(dataFromService) {
+        console.log(dataFromService);
+
+
+        $scope.housingTokyo = dataFromService.categories[0];
+        console.log($scope.housingTokyo);
+
+        $scope.costOfLivingTokyo = dataFromService.categories[1];
+        console.log($scope.cost0fLivingTokyo);
+
+
+        $scope.startupsTokyo = dataFromService.categories[2];
+        console.log($scope.startupsTokyo);
+
+
+      $scope.ventureCapitalTokyo = dataFromService.categories[3];
+      console.log($scope.ventureCapitalTokyo);
+
+      });
+    };
+
+
+    $scope.getInfoTokyo();
+
+
+
 
   });
--- js-main/service.js
+++ js-main/service.js
@@ -10,5 +10,27 @@ angular.module("app").service("service", function($http){
 
 
 	}
+});
+  angular.module("app").service("serviceBerlin", function($http){
+    this.getInfoBerlin = function(){
 
+  return  $http({
+        method: 'GET',
+        url: 'https://api.teleport.org/api/urban_areas/slug:berlin/scores/'
+    }).then(function(response) {var parsedResponse = response.data.Economy;
+      return response.data;
+		})
+}
+});
+
+  angular.module("app").service("serviceTokyo", function($http){
+    this.getInfoTokyo = function(){
+
+  return  $http({
+        method: 'GET',
+        url: 'https://api.teleport.org/api/urban_areas/slug:tokyo/scores/'
+    }).then(function(response) {var parsedResponse = response.data.Economy;
+      return response.data;
+		})
+}
 });
